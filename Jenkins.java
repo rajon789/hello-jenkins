cfdc3e007f5cad2e57ecab00cceb30ee9fa7db4e
Md. Mahmudul Haq <haq.ntl@nazihar.com>
2021-11-11 17:41:19 +0600

    Updated

---
+++ Jenkins.java
@@ -0,0 +1,10 @@
+public class Jenkins {
+    public static void main(String[] args) {
+        System.out.println("Jenkins test automotion");
+        System.out.println("__________________________");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Jenkins pipeline");
+        }
+        System.out.println("Hello, Jenkins");
+    }
+}
